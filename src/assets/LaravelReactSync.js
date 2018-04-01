147dbb5beeff383f364f263228d89e1693c5e1d5
Sean Bethel <tech@jmbdc.com>
2018-04-01 15:40:14 -0400

    make alerts dismiss automatically

--- src/assets/LaravelReactSync.js
+++ src/assets/LaravelReactSync.js
@@ -158,10 +158,23 @@ export class MasterComponent extends Component{
 
 
 export class Alert extends Component{
+	constructor(props){
+		super(props);
+		this.state = {show: true};
+	}
+
+	componentDidMount(){
+		setTimeout(() => {
+			$(ReactDOM.findDOMNode(this)).alert('close').on('closed.bs.alert', () => {
+			  // this.setState({show: false});
+			});
+		}, 3000);
+
+	}
 	
 	render() {
 		
-		return <div className={`alert fade show alert-${this.props.level || 'success'}`}>{this.props.message} <a className="close text-muted" data-dismiss="alert"></a></div>;
+		return this.state.show ? <div style={{position: 'fixed', margin: 'auto', left: 0, right: 0, zIndex: '99999'}} className={`alert fade show alert-${this.props.level || 'success'}`}>{this.props.message} <a className="close text-muted" data-dismiss="alert"></a></div> : null;
 
 	}	
 	
