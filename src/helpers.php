47c2aeb105e44626e0ad97b02db1afaa335388f6
Sean Bethel <g3n1us@users.noreply.github.com>
2018-06-11 22:53:41 -0400

    Update helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,21 @@
 <?php
 	
+	if(!function_exists('current_route')){
+		function current_route(){
+			return collect(\Route::current())
+				->map(function($v, $k){
+					return [$k, $v];
+				})
+				->reduce(function ($coll, $v) {
+					$k = preg_replace('/[^\w]/', '', $v[0]);
+					$coll->put($k, $v[1]);
+				    return $coll;
+				}, collect([]));
+
+		}
+	}
+
+
 	if(!function_exists('get_user_abilities')){
 		function get_user_abilities(){
 
